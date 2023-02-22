493afef98574c7787bd9065654c08cba8f523bed
juanda <juan.pino@ucp.edu.co>
2023-02-22 00:35:13 -0500

    update cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
 
-import { Fragment, useState } from "react"
-import { Cart } from "./Cart";
-import { Precart } from "./Precart";
-import { Product } from "./Product"
+import { Fragment, useState } from "react";
+import { Product } from "./Product";
 
-import productosJson from './productos.json'
 
 export const App = () => {
 
-    let productos = productosJson;
+
 
   return (
     <Fragment>
-
-        <h1 className="titulo-principal">Productos</h1>
-
-        <Precart/>
-
-        <div className="cards"> 
-            <Product productos={productos} />
-        </div> 
-
+      <h1 className="titulo-principal">Productos</h1>
+      <div className="cards">
+          <Product/>
+      </div>
     </Fragment>
-
-  )
-}
+  );
+};
